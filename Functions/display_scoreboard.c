69850029fe65ac76888675a4a4e6447c68fc2d54
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-11-27 19:09:56 +0500

    Create display_scoreboard.c

---
+++ Functions/display_scoreboard.c
@@ -0,0 +1,14 @@
+void display_scoreboard(Player scoreboard[])
+{
+    printf("\nSCOREBOARD:\n");
+    printf("=============================\n");
+    printf("Rank\tName\t\tScore\n");
+    printf("=============================\n");
+
+    for (int i = 0; i < MAX_SCORES; i++)
+    {
+        printf("%d.\t%s\t\t%d\n", i + 1, scoreboard[i].name, scoreboard[i].score);
+    }
+
+    printf("=============================\n");
+}
